121f4024ed1649b5360dffe460ed8e501c193aec
vangaru <ivanenko0607@gmail.com>
2021-10-24 13:22:25 +0300

    feat: global filtering

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import {DataViewModule} from "primeng/dataview";
 import {ButtonModule} from "primeng/button";
 import {InputTextModule} from "primeng/inputtext";
 import { AddProductFormComponent } from './add-product-form/add-product-form.component';
+import {KeyFilterModule} from "primeng/keyfilter";
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { AddProductFormComponent } from './add-product-form/add-product-form.com
         TableModule,
         DataViewModule,
         ButtonModule,
-        InputTextModule
+        InputTextModule,
+        KeyFilterModule
     ],
   providers: [],
   bootstrap: [AppComponent]
